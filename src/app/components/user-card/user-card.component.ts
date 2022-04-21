9e45299c0a6fc75e9577ea977e39463de5090047
NessaAz <vanessa.azenwa@gmail.com>
2022-04-21 12:54:50 +0300

    add user card component code elements

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { RequestLimitService } from 'src/app/services/request-limit.service';
+
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.css']
+  styleUrls: ['./user-card.component.css'],
 })
 export class UserCardComponent implements OnInit {
+  user!: User;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private requestLimitService: RequestLimitService
+  ) {}
 
   ngOnInit(): void {
+    this.userService.user.subscribe((user) => {
+      this.user = user;
+      this.requestLimitService.getRequestLimit().subscribe();
+    });
   }
 
+  // getUsers(users: any): void {
+  //   this.users = event;
+  // }
 }
