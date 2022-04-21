ce52b2a59cc6b6cd03d990a7bf7f5fe5088d78e0
NessaAz <vanessa.azenwa@gmail.com>
2022-04-21 12:54:08 +0300

    add model elements

--- src/app/models/repo.model.ts
+++ src/app/models/repo.model.ts
@@ -1,2 +1,12 @@
 export interface Repo {
-}
+    name: string;
+    language: string;
+    created_at: string;
+    licence: string;
+    // description: string;
+    updated_at: string;
+    forks_count: number;
+    watchers_count: number;
+    html_url: string;
+  }
+  
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,2 +1,14 @@
 export interface User {
-}
+    avatar_url: string;
+    login: string;
+    followers: number;
+    following: number;
+    bio: string;
+    twitter_username: string;
+    public_repos: number;
+    location: string;
+    updated_at: any;
+    hireable: boolean;
+    html_url: string;
+  }
+  
