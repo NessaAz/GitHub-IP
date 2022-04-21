373a6519256ff33dacd09115281e9df38a602dcb
NessaAz <vanessa.azenwa@gmail.com>
2022-04-21 12:10:16 +0300

    add elements to & style the search form

--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.css']
+  styleUrls: ['./search-form.component.css'],
 })
 export class SearchFormComponent implements OnInit {
+  username: string = '';
+  @Output() newSearch: EventEmitter<any> = new EventEmitter();
+  showProfileModal: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(username: string): void {
+    if (username.length > 1) {
+      username = username.replace(/\s/g, '').trim().toLocaleLowerCase();
+      this.newSearch.emit(username);
+      this.username = '';
+    }
   }
-
 }
