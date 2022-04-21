b4063720a01a91b5cbcc5814b9fa13fc1cdbf5c3
NessaAz <vanessa.azenwa@gmail.com>
2022-04-21 07:59:22 +0300

    add follower card, followers page, following card page elements

--- src/app/components/follower-card/follower-card.component.ts
+++ src/app/components/follower-card/follower-card.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from 'src/app/services/user.service';
+import { FollowerService } from '../../services/follower.service';
+import { Router } from '@angular/router';
+import { RequestLimitService } from 'src/app/services/request-limit.service';
 @Component({
   selector: 'app-follower-card',
   templateUrl: './follower-card.component.html',
-  styleUrls: ['./follower-card.component.css']
+  styleUrls: ['./follower-card.component.css'],
 })
 export class FollowerCardComponent implements OnInit {
-
-  constructor() { }
+  user: any;
+  followers!: any;
+  constructor(
+    private router: Router,
+    private followerService: FollowerService,
+    private userService: UserService,
+    private requestLimitService: RequestLimitService
+  ) {}
 
   ngOnInit(): void {
+    this.userService.user.subscribe((user) => this.getFollowers(user.login));
   }
 
+  getFollowers(username: string): void {
+    this.followerService
+      .getFollowers(username)
+      .then((followers) => {
+        this.followers = followers;
+        this.requestLimitService.getRequestLimit().subscribe()
+
+      })
+      .catch((error) => {
+        console.log('Followers search error: ', error);
+      });
+  }
 }
--- src/app/components/following-card/following-card.component.ts
+++ src/app/components/following-card/following-card.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FollowingService } from 'src/app/services/following.service';
+import { UserService } from 'src/app/services/user.service';
+import { RequestLimitService } from 'src/app/services/request-limit.service';
 
 @Component({
   selector: 'app-following-card',
   templateUrl: './following-card.component.html',
-  styleUrls: ['./following-card.component.css']
+  styleUrls: ['./following-card.component.css'],
 })
 export class FollowingCardComponent implements OnInit {
-
-  constructor() { }
+  user: any;
+  following!: any;
+  constructor(
+    private followingService: FollowingService,
+    private userService: UserService,
+    private requestLimitService: RequestLimitService
+  ) {}
 
   ngOnInit(): void {
+    this.userService.user.subscribe((user) => this.getFollowing(user.login));
   }
 
+  getFollowing(username: string): void {
+    this.followingService.getFollowing(username).then((following) => {
+      this.following = following;
+      this.requestLimitService.getRequestLimit().subscribe();
+    });
+  }
 }
