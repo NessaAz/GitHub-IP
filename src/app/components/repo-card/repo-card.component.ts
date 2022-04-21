20d4ab32bea229f2a7fdeddc1b6359086a7ba7cd
NessaAz <vanessa.azenwa@gmail.com>
2022-04-21 11:54:44 +0300

    add reository card elements

--- src/app/components/repo-card/repo-card.component.ts
+++ src/app/components/repo-card/repo-card.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+
+import { Repo } from 'src/app/models/Repo';
+import { RepoService } from 'src/app/services/repo.service';
+import { RequestLimitService } from 'src/app/services/request-limit.service';
+
 @Component({
   selector: 'app-repo-card',
   templateUrl: './repo-card.component.html',
-  styleUrls: ['./repo-card.component.css']
+  styleUrls: ['./repo-card.component.css'],
 })
 export class RepoCardComponent implements OnInit {
+  repos: Repo[] = [];
+  repoError: any;
 
-  constructor() { }
+  constructor(
+    private repoService: RepoService,
+    private requestLimitService: RequestLimitService,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
+    this.getUsername();
   }
 
+  getUsername(): void {
+    const username = String(this.route.snapshot.paramMap.get('username'));
+    this.repoService
+      .getRepos(username)
+      .then((repos) => {
+        this.repos = repos;
+        this.requestLimitService.getRequestLimit().subscribe();
+      })
+      .catch((error) => (this.repoError = error));
+  }
 }
