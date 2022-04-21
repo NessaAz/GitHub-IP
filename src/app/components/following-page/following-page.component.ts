49f8f07ffd80c80c8c84a88351fabfbeedb25930
NessaAz <vanessa.azenwa@gmail.com>
2022-04-21 11:51:49 +0300

    add landing page elements

--- src/app/components/following-page/following-page.component.ts
+++ src/app/components/following-page/following-page.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FollowingPageComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+    ngOnInit(): void {
   }
 
 }
