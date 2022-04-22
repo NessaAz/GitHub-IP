53c3fd4d25f449a726cb71b5ca73055cdf5075a0
NessaAz <vanessa.azenwa@gmail.com>
2022-04-22 16:02:42 +0300

    fixed bug on environment.ts files production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  githubApi : "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"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   githubApi : "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"
 };
 
